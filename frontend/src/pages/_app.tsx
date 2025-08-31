de7de848aa7d5ce57006110002c5c44108c4ecd7
Varun Singh <113547613+Varun-2538@users.noreply.github.com>
2025-08-31 11:15:23 +0530

    Initial commit: DeFi application from Unite DeFi Platform

---
+++ frontend/src/pages/_app.tsx
@@ -0,0 +1,27 @@
+import type { AppProps } from 'next/app'
+import '@/styles/globals.css'
+import { Toaster } from 'react-hot-toast'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Toaster 
+        position="top-right"
+        toastOptions={{
+          duration: 4000,
+          style: {
+            background: '#363636',
+            color: '#fff',
+          },
+          success: {
+            duration: 3000,
+            theme: {
+              primary: '#4aed88',
+            },
+          },
+        }}
+      />
+    </>
+  )
+}
